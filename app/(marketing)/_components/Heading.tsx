2f989b627b52c893c70527f69d3d4a51db13fbcd
Diego Garcia <dagadev@proton.me>
2024-01-30 21:54:47 -0500

    Demo user

--- app/(marketing)/_components/Heading.tsx
+++ app/(marketing)/_components/Heading.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useState } from "react";
 import { ArrowRight } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { SignInButton } from "@clerk/clerk-react";
@@ -7,8 +8,12 @@ import { useConvexAuth } from "convex/react";
 import Link from "next/link";
 
 export const Heading = () => {
-
     const { isAuthenticated, isLoading } = useConvexAuth();
+    const [demoModal, setDemoModal] = useState(false);
+
+    const openDemoUserModal = () => {
+        setDemoModal(!demoModal);
+    }
 
     return (
         <div className="max-w-3xl space-y-4">
@@ -39,12 +44,40 @@ export const Heading = () => {
             }
             {
                 !isAuthenticated && !isLoading && (
-                    <SignInButton mode="modal">
-                        <Button>
-                            Get Notewave free.
-                            <ArrowRight className="h-4 w-4 ml-2" />
+                    <div className="flex gap-4 flex-wrap mx-auto justify-center items-center">
+                        <SignInButton mode="modal">
+                            <Button>
+                                Get Notewave free.
+                                <ArrowRight className="h-4 w-4 ml-2" />
+                            </Button>
+                        </SignInButton>
+                        <Button onClick={() => openDemoUserModal()}>
+                            Try Demo User
                         </Button>
-                    </SignInButton>
+                    </div>
+                )
+            }
+            {
+                demoModal && (
+                    <div className="fixed inset-0 bg-black bg-opacity-40 flex items-center justify-center z-[99999] p-2">
+                        <div className="bg-white p-8 rounded-lg text-gray-800">
+                            <h2 className="text-2xl font-bold mb-4">Demo User</h2>
+                            <p className="mb-4">Use the following credentials to login as a demo user.</p>
+                            <p className="mb-4">Email:
+                                <span className="font-bold ml-1">
+                                    demo@user.com
+                                </span>
+                            </p>
+                            <p className="mb-4">Password:
+                                <span className="font-bold ml-1">
+                                    demo@user
+                                </span>
+                            </p>
+                            <Button variant="secondary" onClick={() => openDemoUserModal()}>
+                                Close
+                            </Button>
+                        </div>
+                    </div>
                 )
             }
         </div>
--- app/(marketing)/_components/Navbar.tsx
+++ app/(marketing)/_components/Navbar.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { Logo } from "./Logo";
@@ -13,6 +14,12 @@ import { useScrollTop } from "@/hooks/useScrollTop";
 export const Navbar = () => {
     const scrolled = useScrollTop();
     const { isAuthenticated, isLoading} = useConvexAuth();
+    const [demoModal, setDemoModal] = useState(false);
+
+    const openDemoUserModal = () => {
+        setDemoModal(!demoModal);
+    }
+
     return (
         <div className={cn(
             "z-50 bg-background dark:bg-[#1F1F1F] fixed top-0 flex items-center w-full p-6", scrolled && "shadow-sm border-b"
@@ -26,17 +33,22 @@ export const Navbar = () => {
                 }
                 {
                     !isLoading && !isAuthenticated && (
-                    <div className="flex gap-x-2">
-                        <SignInButton mode="modal">
-                            <Button variant="ghost" size="sm">
-                                Log in
-                            </Button>
-                        </SignInButton>
-                        <SignInButton mode="modal">
-                            <Button size="sm">
-                                Get Notewave free
-                            </Button>
-                        </SignInButton>
+                    <div className="flex flex-col md:flex-row gap-2">
+                        <div className="flex gap-x-2">
+                            <SignInButton mode="modal">
+                                <Button variant="ghost" size="sm">
+                                    Log in
+                                </Button>
+                            </SignInButton>
+                            <SignInButton mode="modal">
+                                <Button size="sm">
+                                    Get Notewave free
+                                </Button>
+                            </SignInButton>
+                        </div>
+                        <Button className="py-0 px-4 rounded-lg" onClick={() => openDemoUserModal()}>
+                            Try Demo User
+                        </Button>
                     </div>
                     )
                 }
@@ -51,6 +63,29 @@ export const Navbar = () => {
                             <UserButton 
                                 afterSignOutUrl="/"
                             />
+                        </div>
+                    )
+                }
+                {
+                    demoModal && (
+                        <div className="fixed inset-0 bg-black bg-opacity-40 flex items-center justify-center z-[99999] p-2">
+                            <div className="bg-white p-8 rounded-lg text-gray-800">
+                                <h2 className="text-2xl font-bold mb-4">Demo User</h2>
+                                <p className="mb-4">Use the following credentials to login as a demo user.</p>
+                                <p className="mb-4">Email:
+                                    <span className="font-bold ml-1">
+                                        demo@user.com
+                                    </span>
+                                </p>
+                                <p className="mb-4">Password:
+                                    <span className="font-bold ml-1">
+                                        demo@user
+                                    </span>
+                                </p>
+                                <Button variant="secondary" onClick={() => openDemoUserModal()}>
+                                    Close
+                                </Button>
+                            </div>
                         </div>
                     )
                 }
